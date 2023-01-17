cb67e6fa3a9339dd4a3664f51b9ed1dd6cfbe70e
jonathan047 <barrosjonathan737@gmail.com>
2023-01-16 23:01:30 -0300

    templates

---
+++ templates/static/quarentena/js/quarentena.js
@@ -0,0 +1,10 @@
+('#meuModal').on('shown.bs.modal', function (focus) {
+    ('#meuInput').trigger('focus')
+    
+    container = document.getElementById("TituloModalCentralizado")
+    
+    html =  <div class="modal fade" id="ExemploModalCentralizado" tabindex="-1" role="dialog" aria-labelledby="TituloModalCentralizado" aria-hidden="true"><div class="modal-dialog modal-dialog-centered" role="document"></div><div class="modal-content"> <div class="modal-header"><h5 class="modal-title" id="TituloModalCentralizado">ALERTA!</h5>#</div></div></div>
+        
+    container= innerHTML = html 
+     
+  })
