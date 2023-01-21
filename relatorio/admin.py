2df070fbad2673ce017c94c76517b5f13474a9f0
jonathan047 <barrosjonathan737@gmail.com>
2023-01-21 13:57:43 -0300

    Delete relatorio directory

--- relatorio/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Sobreviventes, Pontos
-
-admin.site.register(Sobreviventes)
-admin.site.register(Pontos)
--- relatorio/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('relatorio_geral/', views.relatorio_geral, name="relatorio_geral"),
-    path('atualiza_sobrevivente/', views.att_sobrevivente, name="atualiza_cliente")
-]
