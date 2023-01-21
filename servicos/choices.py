7c48e2357837ef4f4cad2ee7e662b3315b0f02ed
jonathan047 <barrosjonathan737@gmail.com>
2023-01-21 13:57:58 -0300

    Delete servicos directory

--- servicos/choices.py
+++
@@ -1,8 +0,0 @@
-from django.db.models import TextChoices
-
-
-class ChoicesCategoriaComercio(TextChoices):
-    TROCAR_MED = "TMED", "Trocar por medicação"
-    TROCAR_MUNICAO ="TM", "Trocar munição"
-    TROCAR_AGUA = "TA", "Trocar por água" 
-    TROCAR_ALIMENTO = "TAL","Trocar por alimento"
--- servicos/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('novo_servico/', views.novo_servico, name="novo_servico"),
-    
-]
