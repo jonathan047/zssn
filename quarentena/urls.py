9fad95308a10b61f52e184c37b699950d89ad21b
jonathan047 <barrosjonathan737@gmail.com>
2023-01-21 13:57:25 -0300

    Delete quarentena directory

--- quarentena/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('infectado/', views.infectado, name="infectado"),
-    
-]
--- quarentena/views.py
+++
@@ -1,4 +0,0 @@
-from django.shortcuts import render
-
-def infectado(request):
-    return render(request, 'infectado.html')
